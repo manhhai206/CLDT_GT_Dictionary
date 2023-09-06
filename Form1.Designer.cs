a3d3d2c019dcd776e3252a40f5ada9f3ee2b0e5a
Manh Hai <manhtronghai2062002@gmail.com>
2023-09-06 21:17:03 +0700

    manhhai

---
+++ Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace tudien11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSEARCH = new System.Windows.Forms.Button();
+            this.btnDELETE = new System.Windows.Forms.Button();
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.txtMeanings = new System.Windows.Forms.TextBox();
+            this.btnEDIT = new System.Windows.Forms.Button();
+            this.btnEXIT = new System.Windows.Forms.Button();
+            this.txtTerm = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtExamples = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnADD = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSEARCH
+            // 
+            this.btnSEARCH.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSEARCH.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSEARCH.Location = new System.Drawing.Point(514, 11);
+            this.btnSEARCH.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSEARCH.Name = "btnSEARCH";
+            this.btnSEARCH.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSEARCH.Size = new System.Drawing.Size(89, 31);
+            this.btnSEARCH.TabIndex = 1;
+            this.btnSEARCH.Text = "TRA CỨU";
+            this.btnSEARCH.UseVisualStyleBackColor = true;
+            this.btnSEARCH.Click += new System.EventHandler(this.btnSEARCH_Click);
+            // 
+            // btnDELETE
+            // 
+            this.btnDELETE.Location = new System.Drawing.Point(514, 129);
+            this.btnDELETE.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDELETE.Name = "btnDELETE";
+            this.btnDELETE.Size = new System.Drawing.Size(89, 31);
+            this.btnDELETE.TabIndex = 7;
+            this.btnDELETE.Text = "XÓA TỪ";
+            this.btnDELETE.UseVisualStyleBackColor = true;
+            this.btnDELETE.Click += new System.EventHandler(this.btnDELETE_Click);
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.txtSearchTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearchTerm.Location = new System.Drawing.Point(303, 11);
+            this.txtSearchTerm.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSearchTerm.Multiline = true;
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(170, 31);
+            this.txtSearchTerm.TabIndex = 0;
+            this.txtSearchTerm.TextChanged += new System.EventHandler(this.txtSearchTerm_TextChanged);
+            // 
+            // txtMeanings
+            // 
+            this.txtMeanings.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMeanings.Location = new System.Drawing.Point(76, 172);
+            this.txtMeanings.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMeanings.Multiline = true;
+            this.txtMeanings.Name = "txtMeanings";
+            this.txtMeanings.Size = new System.Drawing.Size(294, 61);
+            this.txtMeanings.TabIndex = 4;
+            // 
+            // btnEDIT
+            // 
+            this.btnEDIT.Location = new System.Drawing.Point(514, 202);
+            this.btnEDIT.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEDIT.Name = "btnEDIT";
+            this.btnEDIT.Size = new System.Drawing.Size(89, 31);
+            this.btnEDIT.TabIndex = 8;
+            this.btnEDIT.Text = "CHỈNH TỪ";
+            this.btnEDIT.UseVisualStyleBackColor = true;
+            this.btnEDIT.Click += new System.EventHandler(this.btnEDIT_Click);
+            // 
+            // btnEXIT
+            // 
+            this.btnEXIT.Location = new System.Drawing.Point(514, 279);
+            this.btnEXIT.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEXIT.Name = "btnEXIT";
+            this.btnEXIT.Size = new System.Drawing.Size(89, 31);
+            this.btnEXIT.TabIndex = 9;
+            this.btnEXIT.Text = "THOÁT";
+            this.btnEXIT.UseVisualStyleBackColor = true;
+            this.btnEXIT.Click += new System.EventHandler(this.btnEXIT_Click);
+            // 
+            // txtTerm
+            // 
+            this.txtTerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTerm.Location = new System.Drawing.Point(76, 69);
+            this.txtTerm.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTerm.Multiline = true;
+            this.txtTerm.Name = "txtTerm";
+            this.txtTerm.Size = new System.Drawing.Size(294, 31);
+            this.txtTerm.TabIndex = 2;
+            // 
+            // txtType
+            // 
+            this.txtType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtType.Location = new System.Drawing.Point(76, 121);
+            this.txtType.Margin = new System.Windows.Forms.Padding(2);
+            this.txtType.Multiline = true;
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(294, 35);
+            this.txtType.TabIndex = 3;
+            // 
+            // txtExamples
+            // 
+            this.txtExamples.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtExamples.Location = new System.Drawing.Point(76, 256);
+            this.txtExamples.Margin = new System.Windows.Forms.Padding(2);
+            this.txtExamples.Multiline = true;
+            this.txtExamples.Name = "txtExamples";
+            this.txtExamples.Size = new System.Drawing.Size(294, 70);
+            this.txtExamples.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Từ cần tìm kiếm :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Từ  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Loại từ :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 202);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Nghĩa  :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 293);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ví dụ :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::tudien11.Properties.Resources.sach;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(114, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnADD
+            // 
+            this.btnADD.Location = new System.Drawing.Point(514, 65);
+            this.btnADD.Margin = new System.Windows.Forms.Padding(2);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(89, 31);
+            this.btnADD.TabIndex = 16;
+            this.btnADD.Text = "THÊM TỪ";
+            this.btnADD.UseVisualStyleBackColor = true;
+            this.btnADD.Click += new System.EventHandler(this.btnADD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 351);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtExamples);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtTerm);
+            this.Controls.Add(this.btnEXIT);
+            this.Controls.Add(this.btnEDIT);
+            this.Controls.Add(this.txtMeanings);
+            this.Controls.Add(this.txtSearchTerm);
+            this.Controls.Add(this.btnDELETE);
+            this.Controls.Add(this.btnSEARCH);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSEARCH;
+        private System.Windows.Forms.Button btnDELETE;
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.TextBox txtMeanings;
+        private System.Windows.Forms.Button btnEDIT;
+        private System.Windows.Forms.Button btnEXIT;
+        private System.Windows.Forms.TextBox txtTerm;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtExamples;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnADD;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static tudien11.Form1;
+
+namespace tudien11
+{
+    public partial class Form1 : Form
+    {
+        public class Word
+        {
+            public string Term { get; set; }
+            public string Type { get; set; }
+            public List<string> Meanings { get; set; }
+            public List<string> Examples { get; set; }
+            public Word Previous { get; set; }
+            public Word Next { get; set; }
+
+            public Word(string term, string type, List<string> meanings, List<string> examples)
+            {
+                Term = term;
+                Type = type;
+                Meanings = meanings;
+                Examples = examples;
+                Previous = null;
+                Next = null;
+
+            }
+            public override string ToString()
+            {
+                return Term;
+            }
+        }
+        public class Dictionary
+        {
+           
+            private Word firstWord;
+            private Word lastWord;
+            private List<Word>[] hashArray;
+
+            public Dictionary()
+            {
+                InitializeDictionary();
+            }
+
+            public void InitializeDictionary()
+            {
+                firstWord = null;
+                lastWord = null;
+                hashArray = new List<Word>[26];
+                for (int i = 0; i < 26; i++)
+                {
+                    hashArray[i] = new List<Word>();
+                }
+
+                LoadDictionaryFromFile("dictionary.txt");
+            }
+
+            public void AddWord(string term, string type, List<string> meanings, List<string> examples)
+            {
+                Word newWord = new Word(term, type, meanings, examples);
+                AddWordToList(newWord);
+                AddWordToHashTable(newWord);
+            }
+
+            public void AddWordToList(Word word)
+            {
+                if (firstWord == null)
+                {
+                    // Adding the first word
+                    firstWord = word;
+                    lastWord = word;
+                }
+                else
+                {
+                    // Adding subsequent words
+                    lastWord.Next = word;
+                    word.Previous = lastWord;
+                    lastWord = word;
+                }
+            }
+
+            public void AddWordToHashTable(Word word)
+            {
+                int index = GetHashIndex(word.Term[0]);
+                hashArray[index].Add(word);
+            }
+
+            public int GetHashIndex(char c)
+            {
+                return char.ToUpper(c) - 'A';
+            }
+
+            public Word SearchWord(string term)
+            {
+                int index = GetHashIndex(term[0]);
+                List<Word> words = hashArray[index];
+                foreach (Word word in words)
+                {
+                    if (word.Term.Equals(term, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return word;
+                    }
+                }
+                return null;
+            }
+
+            public void EditWord(Word word, string newType, List<string> newMeanings, List<string> newExamples)
+            {
+                if (!string.IsNullOrEmpty(newType))
+                {
+                    word.Type = newType;
+                }
+
+                if (newMeanings.Count > 0)
+                {
+                    word.Meanings = newMeanings;
+                }
+
+                if (newExamples.Count > 0)
+                {
+                    word.Examples = newExamples;
+                }
+               
+            }
+
+            public void DeleteWord(Word word)
+            {
+                if (word == firstWord)
+                {
+                    // Deleting the first word
+                    firstWord = word.Next;
+                    if (firstWord != null)
+                    {
+                        firstWord.Previous = null;
+                    }
+                }
+                else if (word == lastWord)
+                {
+                    // Deleting the last word
+                    lastWord = word.Previous;
+                    if (lastWord != null)
+                    {
+                        lastWord.Next = null;
+                    }
+                }
+                else
+                {
+                    // Deleting a word in the middle
+                    word.Previous.Next = word.Next;
+                    word.Next.Previous = word.Previous;
+                }
+
+                int index = GetHashIndex(word.Term[0]);
+                hashArray[index].Remove(word);
+
+                RemoveWordFromFile(word); // Xóa từ trong tệp
+            }
+            public void RemoveWordFromFile(Word word)
+            {
+                string fileName = "dictionary.txt";
+                try
+                {
+                    string[] lines = File.ReadAllLines(fileName);
+                    List<string> updatedLines = new List<string>();
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split('|');
+                        if (parts.Length == 4)
+                        {
+                            string term = parts[0];
+                            if (!term.Equals(word.Term, StringComparison.OrdinalIgnoreCase))
+                            {
+                                updatedLines.Add(line);
+                            }
+                        }
+                    }
+
+                    File.WriteAllLines(fileName, updatedLines);
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error removing the word from the dictionary file.");
+                }
+            }
+
+
+            public void SaveDictionaryToFile(string fileName)
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(fileName))
+                    {
+                        Word currentWord = firstWord;
+                        while (currentWord != null)
+                        {
+                            // Ghi thông tin từ vào tệp
+                            sw.WriteLine($"{currentWord.Term}|{currentWord.Type}|{string.Join(";", currentWord.Meanings)}|{string.Join(";", currentWord.Examples)}");
+
+                            currentWord = currentWord.Next;
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error writing to the dictionary file.");
+                }
+            }
+
+            public void LoadDictionaryFromFile(string fileName)
+            {
+                try
+                {
+                    if (!File.Exists(fileName))
+                    {
+                        // Tạo tệp mới nếu không tồn tại
+                        File.Create(fileName).Close();
+                        return;
+                    }
+
+                    using (StreamReader sr = new StreamReader(fileName))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            string[] parts = line.Split('|');
+                            if (parts.Length == 4)
+                            {
+                                string term = parts[0];
+                                string type = parts[1];
+                                List<string> meanings = new List<string>(parts[2].Split(';'));
+                                List<string> examples = new List<string>(parts[3].Split(';'));
+
+                                AddWord(term, type, meanings, examples);
+                            }
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Error reading the dictionary file.");
+                }
+            }
+        
+
+
+
+    }
+        private Dictionary dictionary;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dictionary = new Dictionary();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnADD_Click(object sender, EventArgs e)
+        {
+            string term = txtTerm.Text.Trim();
+            string type = txtType.Text.Trim();
+            string[] meanings = txtMeanings.Text.Trim().Split(';');
+            string[] examples = txtExamples.Text.Trim().Split(';');
+
+            if (!string.IsNullOrEmpty(term) && !string.IsNullOrEmpty(type))
+            {
+                // Kiểm tra xem từ đã tồn tại trong từ điển hay chưa
+                Word existingWord = dictionary.SearchWord(term);
+                if (existingWord != null)
+                {
+                    MessageBox.Show("Từ đã tồn tại trong từ điển.");
+                    ClearFields(); // Xóa nội dung các trường văn bản
+                    return;
+                }
+
+                List<string> meaningList = new List<string>(meanings);
+                List<string> exampleList = new List<string>(examples);
+
+                dictionary.AddWord(term, type, meaningList, exampleList);
+                ClearFields();
+                MessageBox.Show("Từ đã được thêm thành công.");
+
+                // Lưu từ điển vào tệp
+                dictionary.SaveDictionaryToFile("dictionary.txt");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập đủ thông tin từ và loại từ.");
+            }
+        }
+
+        private void btnSEARCH_Click(object sender, EventArgs e)
+        {
+            string term = txtSearchTerm.Text;
+            Word word = dictionary.SearchWord(term);
+
+            if (!string.IsNullOrEmpty(term))
+            {
+                if (word != null)
+                {
+                    // Hiển thị thông tin từ tìm kiếm
+                    txtTerm.Text = word.Term;
+                    txtType.Text = word.Type;
+
+                    // Hiển thị nghĩa
+                    txtMeanings.Text = string.Join("; ", word.Meanings);
+
+                    // Hiển thị ví dụ
+                    txtExamples.Text = string.Join("; ", word.Examples);
+                }
+                else
+                {
+                    MessageBox.Show("Từ không tồn tại trong từ điển.");
+                    ClearFields();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập từ cần tra.");
+            }
+            txtSearchTerm.Focus();
+            txtSearchTerm.Clear();
+        }
+
+        private void btnDELETE_Click(object sender, EventArgs e)
+        {
+            string term = txtTerm.Text.Trim();
+
+            Word word = dictionary.SearchWord(term);
+            if (word == null)
+            {
+                MessageBox.Show("Từ không tồn tại.");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn xóa từ này?", "Xác nhận", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                dictionary.DeleteWord(word);
+                ClearFields();
+                MessageBox.Show("Từ đã được xóa thành công.");
+            }
+        }
+        public void ClearFields()
+        {
+            txtTerm.Text = string.Empty;
+            txtType.Text = string.Empty;
+            txtMeanings.Text = string.Empty;
+            txtExamples.Text = string.Empty;
+        }
+
+        private void btnEDIT_Click(object sender, EventArgs e)
+        {
+            string term = txtTerm.Text.Trim();
+            string type = txtType.Text.Trim();
+            string[] meanings = txtMeanings.Text.Trim().Split(';');
+            string[] examples = txtExamples.Text.Trim().Split(';');
+
+            if (!string.IsNullOrEmpty(term))
+            {
+                Word word = dictionary.SearchWord(term);
+
+                if (word != null)
+                {
+                    // Chỉnh sửa thông tin từ
+                    dictionary.EditWord(word, type, new List<string>(meanings), new List<string>(examples));
+                    MessageBox.Show("Từ đã được chỉnh sửa thành công.");
+
+                    // Xóa nội dung các trường văn bản
+                 
+                    ClearFields();
+                    dictionary.SaveDictionaryToFile("dictionary.txt");
+                }
+                else
+                {
+                    MessageBox.Show("Từ không tồn tại trong từ điển.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập từ cần chỉnh sửa.");
+            }
+        }
+
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtSearchTerm_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
+
